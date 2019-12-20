b39eb1fdc0e689770bae80abd49b84adb1f539e8
why <861466478@qq.com>
2019-12-20 14:39:31 +0800

    2019-12-20

--- tryme/lianshou2.py
+++ tryme/lianshou2.py
@@ -3,11 +3,12 @@
 
 import re
 # 第三题 输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
-# 本题应该为目前网上最简单，最迅速的方法哈（求大佬喷）
+
 # 统计字母、空格、数字这些东西，能迅速把他们进行分类的，只有正则表达式
 # 正则表达式挑选符合要求的数字，以后爬虫也会经常遇到，就当练手
+
 import re
-a = str(input('请输入一串字符：'))
+a = raw_input('请输入一串字符：')
 # 注意不要忽略了引号
 # re.compile()编译正则表达式，生成正则表达式对象
 zimu = re.compile('[a-zA-Z]')
@@ -19,6 +20,8 @@ count_zimu = len(zimu.findall(a))
 count_kongge = len(kongge.findall(a))
 count_qita = len(a) - count_kongge - count_zimu - count_shuzi
 print('英文字母个数为：%d'%count_zimu)
+b=shuzi.findall(a)
+print (b)
 print('空格个数为：%d'%count_kongge)
 print('数字个数为：%d'%count_shuzi)
 print('其他个数为：%d'%count_qita)
---
+++ tryme/lianshou3.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# encoding:utf_8
+
+# 第四题 两个乒乓球队进行比赛，各出三人。甲队为a,b,c三人，乙队为x,y,z三人。已抽签决定
+# 比赛名单。有人向队员打听比赛的名单。a说他不和x比，c说他不和x,z比，请编程序找出三队赛手的名单。
+
+# 这道题用到很多重新赋值，以及列表删除方法
+# 首先注意a不能直接赋值
+
+team_jia = [str('a'), str('b'), str('c')]
+# 第一个循环确定a和谁比赛，
+for i in range(0, 3):
+    team_yi = [str('x'), str('y'), str('z')]
+    match_list = []
+    selected_first_yi = team_yi.pop(i)
+    if selected_first_yi != 'x':
+        # 第二个循环，产生接下来的两场比赛
+        for j in range(0, 2):
+            # 由于后面又用到了list.pop()，因此要重新赋值
+            team_yi = [str('x'), str('y'), str('z')]
+            team_yi.remove(selected_first_yi)
+            # 重新赋值
+            match_list = [tuple([team_jia[0], selected_first_yi])]
+            match_list.append(tuple([team_jia[1], team_yi.pop(j)]))
+            # 两次pop()后，team_yi只剩下一个值
+            match_list.append(tuple([team_jia[2], team_yi[0]]))
+            if ('c', 'x') not in match_list and ('c', 'z') not in match_list :
+                print("比赛顺序为:", match_list, '.')
